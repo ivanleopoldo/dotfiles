3c543651c3d497e8bee3db7ebcc8b11194bc690d
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-05-23 15:55:27 +0800

    removed config in options; added mkdnflow configurator

--- nvim/lua/plugins/mkdnflow.lua
+++ nvim/lua/plugins/mkdnflow.lua
@@ -3,6 +3,10 @@ return {
   config = function()
     require("mkdnflow").setup({
       -- Config goes here; leave blank for defaults
+      perspective = {
+        priority = "root",
+        root_tell = "index.md",
+      },
     })
   end,
 }
