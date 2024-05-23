8bd38b07cc88176600841c644fc871ccbbb14796
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-05-23 15:42:40 +0800

    added mkdnflow; removed simplenote.vim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,8 +2,3 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-
-local conf = require("private.simplenote")
-
-vim.g.SimplenoteUsername = conf.Username
-vim.g.SimpleNotePassword = conf.Password
