886512216458d94685849379d239bda2a0b7387f
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-09-16 20:24:26 +0800

    added zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,19 @@
 # Simple Aliases
 
+# Overriding default cmd
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias lst="eza --tree --level=2 --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias cat="bat"
+alias e="exit"
+alias c="clear"
+
+# zsh
+alias szsh="source ~/.zshrc"
+alias zshrc="nvim ~/.zshrc"
+
+# Nvim
+alias nvim="nvim ."
+alias vi="nvim ."
 
 # Python
 alias python="python3"
@@ -14,7 +27,3 @@ alias tks="tmux kill-server"
 alias tk="tmux kill-sess -t "
 alias ta="tmux a -t "
 alias tsc="tmux source ~/.config/tmux/tmux.conf"
-
-# Sytem
-alias e="exit"
-alias c="clear"
