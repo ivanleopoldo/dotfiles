a75bd25200fbd787f76d5cf4b54013589a129095
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-12-15 18:28:32 +0800

    adds workspace switching with trackpad gestures feature using hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,25 @@
+Swipe = hs.loadSpoon("Swipe")
+
+local AEROSPACE = "/opt/homebrew/bin/aerospace"
+
+function aerospaceExec(cmd)
+	os.execute("nohup " .. AEROSPACE .. " " .. cmd .. " &")
+end
+
+-- use four finger swipe to switch workspace
+local current_id, threshold
+Swipe:start(4, function(direction, distance, id)
+	if id == current_id then
+		if distance > threshold then
+			threshold = math.huge -- only trigger once per swipe
+			if direction == "left" then
+				aerospaceExec("workspace --wrap-around next")
+			elseif direction == "right" then
+				aerospaceExec("workspace --wrap-around prev")
+			end
+		end
+	else
+		current_id = id
+		threshold = 0.001 -- swipe distance > 10% of trackpad
+	end
+end)
