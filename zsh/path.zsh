fc9bf626dce57907f5299b3858ab3a185f64fd39
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-12-17 21:41:52 +0800

    chore(zsh): adds XDG_CONFIG_PATH to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,6 +4,9 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
+# XDG_CONFIG_PATH
+export XDG_CONFIG_HOME="$HOME/.config"
+
 # FVM
 export PATH="$PATH:/Users/ivanleopoldo/fvm/default/bin"
 
